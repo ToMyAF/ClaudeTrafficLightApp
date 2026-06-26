20878657188a13cfc7c8150b54559e02381b0c72
阿锋i <1197827679@qq.com>
2026-06-26 18:27:59 +0800

    feat: init WPF project with basic draggable window (Note: build requires proper Windows SDK access)

---
+++ TrafficLightApp/App.xaml.cs
@@ -0,0 +1,5 @@
+﻿using System.Windows;
+namespace ClaudeTrafficLight;
+public partial class App : Application
+{
+}
---
+++ TrafficLightApp/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Input;
+namespace ClaudeTrafficLight;
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        MouseLeftButtonDown += (s, e) => DragMove();
+    }
+}
